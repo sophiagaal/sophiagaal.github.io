2844454ec2405c0c4c8344197f94dc39104ff26a
Mustafa Sameen <s_sameen@coloradocollege.edu>
2025-03-09 19:34:59 -0600

    updated website

--- libs/custom/my_js.js
+++ libs/custom/my_js.js
@@ -1,28 +1,27 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   // Variables
-  var $codeSnippets = $('.code-example-body'),
-      $nav = $('.navbar'),
-      $body = $('body'),
-      $window = $(window),
-      $popoverLink = $('[data-popover]'),
-      navOffsetTop = $nav.offset().top,
-      $document = $(document),
-      entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': '&quot;',
-        "'": '&#39;',
-        "/": '&#x2F;'
-      }
+  var $codeSnippets = $(".code-example-body"),
+    $nav = $(".navbar"),
+    $body = $("body"),
+    $window = $(window),
+    $popoverLink = $("[data-popover]"),
+    navOffsetTop = $nav.offset().top,
+    $document = $(document),
+    entityMap = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&#39;",
+      "/": "&#x2F;",
+    };
 
   function init() {
-    $window.on('scroll', onScroll)
-    $window.on('resize', resize)
-    $popoverLink.on('click', openPopover)
-    $document.on('click', closePopover)
-    $('a[href^="#"]').on('click', smoothScroll)
+    $window.on("scroll", onScroll);
+    $window.on("resize", resize);
+    $popoverLink.on("click", openPopover);
+    $document.on("click", closePopover);
+    $('a[href^="#"]').on("click", smoothScroll);
     buildSnippets();
   }
 
@@ -30,48 +29,64 @@ $(document).ready(function() {
     e.preventDefault();
     $(document).off("scroll");
     var target = this.hash,
-        menu = target;
+      menu = target;
     $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-40
-    }, 0, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top - 40,
+        },
+        0,
+        "swing",
+        function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+        }
+      );
   }
 
   function openPopover(e) {
-    e.preventDefault()
+    e.preventDefault();
     closePopover();
-    var popover = $($(this).data('popover'));
-    popover.toggleClass('open')
+    var popover = $($(this).data("popover"));
+    popover.toggleClass("open");
     e.stopImmediatePropagation();
   }
 
   function closePopover(e) {
-    if($('.popover.open').length > 0) {
-      $('.popover').removeClass('open')
+    if ($(".popover.open").length > 0) {
+      $(".popover").removeClass("open");
     }
   }
 
-  $("#button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#elementtoScrollToID").offset().top
-    }, 2000);
-});
+  $("#button").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#elementtoScrollToID").offset().top,
+      },
+      2000
+    );
+  });
 
   function resize() {
-    $body.removeClass('has-docked-nav')
-    navOffsetTop = $nav.offset().top
-    onScroll()
+    $body.removeClass("has-docked-nav");
+    navOffsetTop = $nav.offset().top;
+    onScroll();
   }
 
   function onScroll() {
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
-      $body.addClass('has-docked-nav')
+    if (
+      navOffsetTop < $window.scrollTop() &&
+      !$body.hasClass("has-docked-nav")
+    ) {
+      $body.addClass("has-docked-nav");
     }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-      $body.removeClass('has-docked-nav')
+    if (
+      navOffsetTop > $window.scrollTop() &&
+      $body.hasClass("has-docked-nav")
+    ) {
+      $body.removeClass("has-docked-nav");
     }
   }
 
@@ -82,13 +97,11 @@ $(document).ready(function() {
   }
 
   function buildSnippets() {
-    $codeSnippets.each(function() {
-      var newContent = escapeHtml($(this).html())
-      $(this).html(newContent)
-    })
+    $codeSnippets.each(function () {
+      var newContent = escapeHtml($(this).html());
+      $(this).html(newContent);
+    });
   }
 
-
   init();
-
 });
